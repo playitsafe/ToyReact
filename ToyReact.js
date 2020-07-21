d14d23f3062b5f91483ff9c398b0cabf351ba02b
aaron <playitsafe@qq.com>
2020-07-21 17:56:25 +1000

    create element

--- ToyReact.js
+++ ToyReact.js
@@ -9,6 +9,10 @@ export let ToyReact = {
       element.setAttribute(name, attributes[name])
     }
     for (let child of children) {
+      if (typeof child === "string") {
+        child = document.createTextNode(child);
+      }
+
       element.appendChild(child);
       
     }
--- main.js
+++ main.js
@@ -1,12 +1,23 @@
-import { ToyReact } from './ToyReact';
+//import { ToyReact } from './ToyReact';
 
 
 let a = <div name="a" id="ida">
-          <span></span>
-          <span></span>
-          <span></span>
+          <span>Hello</span>
+          <span>World</span>
+          <span>!!!</span>
         </div>
 
 //console.log(a)
+/*
+var a = ToyReact.createElement(
+  "div", 
+  {
+    name: "a",
+    id: "ida"
+  }, 
+  ToyReact.createElement("span", null, "Hello"), 
+  ToyReact.createElement("span", null, "World"), 
+  ToyReact.createElement("span", null, "!!!")); 
+*/
 
 document.body.appendChild(a);
